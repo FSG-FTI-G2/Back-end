dd4c9cdffa5f59f558ed1c11f30a0a8f834aaf18
Ming Doan <quangminh57dng@gmail.com>
2024-10-01 15:54:27 +0700

    feat: Add reponse object & error exception

---
+++ app/utils/response.py
@@ -0,0 +1,21 @@
+import time
+from typing import Optional, Any
+from pydantic import BaseModel, Field
+
+
+class ResponseModel(BaseModel):
+    code: int = Field(None, alias="code")
+    message: str = Field(None, alias="message")
+    data: Optional[Any] = Field(None, alias="data")
+    errors: Optional[Any] = Field(None, alias="errors")
+    timestamp: int = Field(None, alias="timestamp")
+
+
+def response(code: int, message: str, data: Optional[Any] = None, error: Optional[Any] = None) -> dict:
+    return ResponseModel(
+        code=code,
+        message=message,
+        data=data,
+        error=error,
+        timestamp=int(time.time())
+    )
