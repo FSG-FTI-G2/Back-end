aea93c1986fdd030e2f6351db44bb4245fae8121
kiwihner <doanquochoan57@gmail.com>
2024-10-02 11:08:26 +0700

    Feature: Done

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,10 +1,8 @@
 from fastapi import APIRouter
-
+from .auth import router as auth_router  
 
 router = APIRouter(prefix="/api/v1", tags=["v1"])
 
 # Include routes
-from .auth import router as auth_router  
-
 
 router.include_router(auth_router)
