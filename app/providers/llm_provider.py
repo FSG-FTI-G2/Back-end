18d3020f432058a2229fe06b56524b417dbb8706
Ming Doan <quangminh57dng@gmail.com>
2024-10-31 10:26:23 +0700

    fix: Fix LangChain Provider Error

--- app/providers/llm_provider.py
+++ app/providers/llm_provider.py
@@ -1,66 +1,126 @@
-import enum
-from typing import TypeVar, Type, TypedDict, Literal, List, Generator, Callable, Dict
-from pydantic import BaseModel
+from typing import TypeVar, Type, List, Generator, Callable
 from typing_extensions import deprecated
+from pydantic import BaseModel
+import enum
+import json
 from fastapi import HTTPException
+from langchain_core.runnables import Runnable
 from langchain.schema import BaseMessage, AIMessage, HumanMessage, SystemMessage
-from langchain.llms import BaseLLM
 from app.utils.prompt_template import RolePrompt, get_prompt_by_role
-from app.configs.llm import GPTClient, GeminiClient, OllamaClient
+from app.configs.llm_config import gpt, azure_gpt, gemini, ollama
 from app.utils.logger import get_logger
 
 _T = TypeVar("T", bound=BaseModel)
 logger = get_logger("LLM", color=96)
 
-# Define the response schema for Gemini
-class GeminiNativeResponse(TypedDict):
-    content: Dict[Literal["parts"], List[Dict[Literal["text"], str]]]
-    finish_reason: int
-    index: int
-    safety_ratings: List
-    token_count: int
-    grounding_attributions: List
-    block_reason: int
-    usage_metadata: Dict[Literal["prompt_token_count"] |
-                         Literal["candidates_token_count"] | Literal["total_token_count"], int]
 
 # Enum for model selection
 class LLMModel(str, enum.Enum):
     GPT = "gpt"
+    AZURE_GPT = "azure_gpt"
     GEMINI = "gemini"
     OLLAMA = "ollama"
 
+
 # Default settings
-DEFAULT_MODEL = LLMModel.GEMINI
-DEFAULT_ROLE = RolePrompt.EXPERT
+DEFAULT_MODEL = LLMModel.OLLAMA
+DEFAULT_ROLE = RolePrompt.GENERAL
+
+PROMPT_STRUCTURE_TEMPLATE = '''
+---
+Here's a JSON schema to follow:
+{schema}
+
+Output a valid JSON object but do not repeat the schema.
+Do not include any other information in the output.
+'''
+FAILED_RESPONSE = "Sorry, I can't answer your question at the moment. Please try again later."
+
+
 
 # Wrappers for each client to standardize the response format
-class GPTClientWrapper(BaseLLM):
-    async def agenerate(self, messages: List[BaseMessage]) -> str:
-        prompt = "\n".join([msg.content for msg in messages])
-        return await GPTClient().generate(prompt)
+class ModelWrapper:
+    '''
+    Model Wrapper for control response format and output for any LLM model.\n
+    This class fixed the drawback of LangChain
+    '''
+    def __init__(self, model: Runnable, max_retry: int = 1):
+        self.model = model
+        self.structure_prompt_modifier: _T = None
+        self.max_retry = max_retry
 
-class GeminiClientWrapper(BaseLLM):
-    async def agenerate(self, messages: List[BaseMessage]) -> str:
-        prompt = "\n".join([msg.content for msg in messages])
-        return await GeminiClient().generate(prompt)
+    def with_structured_output(self, parser: Type[_T]) -> 'ModelWrapper':
+        # If the model has a method with_structured_output, use it
+        if 'with_structured_output' in self.model.__dict__ and callable(self.model.__dict__['with_structured_output']):
+            self.model = self.model.with_structured_output(parser)
+        else:
+            # Else, return a model, however, enable structured prompt modifier
+            self.structure_prompt_modifier = parser
+        return self
 
-class OllamaClientWrapper(BaseLLM):
-    async def agenerate(self, messages: List[BaseMessage]) -> str:
-        prompt = "\n".join([msg.content for msg in messages])
-        return await OllamaClient().generate(prompt)
+    async def __ainvoke(self, messages: List[BaseMessage]) -> str | BaseModel:
+        # Modify the last message if needed
+        if self.structure_prompt_modifier:
+            messages[-1].content += PROMPT_STRUCTURE_TEMPLATE.format(
+                schema=self.structure_prompt_modifier.model_json_schema())
+
+        result = await self.model.ainvoke(messages)
+
+        # If the model is structured, parse the output
+        if self.structure_prompt_modifier:
+            if isinstance(result, str):
+                result = json.loads(result)
+            return self.structure_prompt_modifier.model_validate(result)
+        return result
+    
+    async def ainvoke(self, messages: List[BaseMessage]) -> str | BaseModel:
+        while self.max_retry > 0:
+            try:
+                return await self.__ainvoke(messages)
+            except Exception as e:
+                logger(f"{self.model.__class__.__name__} error: {e}. Retrying...")
+                self.max_retry -= 1
+        return FAILED_RESPONSE
+    
+    def __invoke(self, messages: List[BaseMessage]) -> str | BaseModel:
+        # Modify the last message if needed
+        if self.structure_prompt_modifier:
+            messages[-1].content += PROMPT_STRUCTURE_TEMPLATE.format(
+                schema=self.structure_prompt_modifier.model_json_schema())
+            
+        result = self.model.invoke(messages)
+        
+        # If the model is structured, parse the output
+        if self.structure_prompt_modifier:
+            if isinstance(result, str):
+                result = json.loads(result)
+            return self.structure_prompt_modifier.model_validate(result)
+        return result
+    
+    def invoke(self, messages: List[BaseMessage]) -> str | BaseModel:
+        while self.max_retry > 0:
+            try:
+                return self.__invoke(messages)
+            except Exception as e:
+                logger(f"{self.model.__class__.__name__} error: {e}. Retrying...")
+                self.max_retry -= 1
+        return FAILED_RESPONSE
+
+
 
 # Main provider class for LLM selection and structured response handling
 class LLMProvider:
     def __init__(self): ...
 
-    def __get_llm_model(self, model: LLMModel) -> BaseLLM:
+    def __get_llm_model(self, model: LLMModel) -> ModelWrapper:
         if model == LLMModel.GPT:
-            return GPTClientWrapper()
+            return ModelWrapper(gpt)
+        elif model == LLMModel.AZURE_GPT:
+            return ModelWrapper(azure_gpt)
         elif model == LLMModel.GEMINI:
-            return GeminiClientWrapper()
+            return ModelWrapper(gemini, max_retry=2)
         elif model == LLMModel.OLLAMA:
-            return OllamaClientWrapper()
+            return ModelWrapper(ollama, max_retry=5)
         else:
             raise HTTPException(status_code=400, detail="Invalid model name")
 
@@ -82,23 +142,25 @@ class LLMProvider:
     ) -> _T:
         # Select the LLM model
         selected_llm = self.__get_llm_model(model_name)
+        structured_llm = selected_llm.with_structured_output(parser)
 
         # Prune the history if needed
         if max_history:
             history = self.__prune_history(history, max_history)
-        
+
         # Add the new question to history
         history.append(HumanMessage(content=question))
-        new_message = HumanMessage(content=f"{context or ''} Prompt: {question}")
+        new_message = HumanMessage(
+            content=f"{context or ''}\nPrompt: {question}")
 
         # Format the messages and send to LLM
         messages = [self.__system_message(role), *history[:-1], new_message]
-        output = await selected_llm.agenerate(messages=messages)
-        
+        output = await structured_llm.ainvoke(messages)
+
         # Parse and return the structured response
-        history.append(AIMessage(content=output))
+        history.append(AIMessage(content=str(output)))
         logger(f"Structured response: {output}")
-        return parser.parse_raw(output)
+        return output
 
     @deprecated("Structured streaming is not supported yet by LangChain")
     def stream_structured_response(
@@ -118,10 +180,11 @@ class LLMProvider:
         # Prune the history if needed
         if max_history:
             history = self.__prune_history(history, max_history)
-        
+
         # Prepare new message and history
         history.append(HumanMessage(content=question))
-        new_message = HumanMessage(content=f"{context or ''} Prompt: {question}")
+        new_message = HumanMessage(
+            content=f"{context or ''} Prompt: {question}")
 
         # Send to LLM with streaming
         messages = [self.__system_message(role), *history[:-1], new_message]
@@ -154,20 +217,22 @@ class LLMProvider:
         # Prune history if needed
         if max_history:
             history = self.__prune_history(history, max_history)
-        
+
         # Add question to history and create message
         history.append(HumanMessage(content=question))
-        new_message = HumanMessage(content=f"{context or ''} Prompt: {question}")
+        new_message = HumanMessage(
+            content=f"{context or ''} Prompt: {question}")
 
         # Send to LLM and get response
         messages = [self.__system_message(role), *history[:-1], new_message]
-        output = await selected_llm.agenerate(messages=messages)
-        
+        output = await selected_llm.ainvoke(messages=messages)
+
         # Update history and log
         history.append(AIMessage(content=output))
         logger(f"Response: {output}")
         return output
 
+    @deprecated("Streaming is not supported yet by LangChain")
     def stream_response(
         self,
         question: str,
@@ -184,10 +249,11 @@ class LLMProvider:
         # Prune history if needed
         if max_history:
             history = self.__prune_history(history, max_history)
-        
+
         # Add question to history and create message
         history.append(HumanMessage(content=question))
-        new_message = HumanMessage(content=f"{context or ''} Prompt: {question}")
+        new_message = HumanMessage(
+            content=f"{context or ''} Prompt: {question}")
 
         # Stream response from LLM
         messages = [self.__system_message(role), *history[:-1], new_message]
